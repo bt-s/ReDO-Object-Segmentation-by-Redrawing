5c7a23dd40e30d6ee16f8ec1454b313b58b3fbd5
Adrian Chmielewski-Anders <adrianc-a@users.noreply.github.com>
2019-10-08 16:58:54 +0200

    Almost finishing faces, discuss differences later
    To discuss: to use all the images? Or a subset as in the paper?

--- get_datasets.py
+++ get_datasets.py
@@ -18,15 +18,20 @@ Datasets/
 the split file contains two fields per line, the number of the image, and the
 type (train, test, validate) which is defined in datasets.Dataset.SPLIT_KEYS
 In paths.txt there are two fields per line, the number of the image and the
-path assuming the common structure shown above.
+path assuming the common structure shown above. Though differences may appear in
+each subclass of dataset.Dataset
 """
 
-from pathlib import Path
 from tensorflow.keras.utils import get_file
 from scipy.io import loadmat
+from PIL import Image
+
 from operator import itemgetter
 from argparse import ArgumentParser
 from sys import argv
+from pathlib import Path
+from shutil import rmtree
+from os.path import splitext
 
 import datasets
 
@@ -37,6 +42,12 @@ def parse_download_args():
     parser.add_argument('-b', '--birds', action='store_true')
     parser.add_argument('-t', '--faces', action='store_true')
     return parser.parse_args(argv[1:])
+
+
+def _rm_dirs(dirs_to_rm):
+    for a_dir in dirs_to_rm:
+        if a_dir.exists():
+            rmtree(a_dir)
 
 
 def configure_flowers(datasets_dir):
@@ -73,9 +84,9 @@ def configure_flowers(datasets_dir):
 
     flowers_split_inds = []
     add_to_inds(flowers_split_inds, datasets.Dataset.SPLIT_KEYS['training'],
-                flowers_splits['trnid'][0].tolist())
-    add_to_inds(flowers_split_inds, datasets.Dataset.SPLIT_KEYS['test'],
                 flowers_splits['tstid'][0].tolist())
+    add_to_inds(flowers_split_inds, datasets.Dataset.SPLIT_KEYS['test'],
+                flowers_splits['trnid'][0].tolist())
     add_to_inds(flowers_split_inds, datasets.Dataset.SPLIT_KEYS['validation'],
                 flowers_splits['valid'][0].tolist())
 
@@ -107,9 +118,116 @@ def configure_flowers(datasets_dir):
     for label in labels_path.iterdir():
         label.rename(str(label).replace('segmim', 'label'))
 
+    _rm_dirs([default_images_path, default_labels_path])
+
+
+def _read_face_split(fname):
+    image_names_in_split = set()
+    with open(fname, 'r') as reader:
+        for line in reader.readlines():
+            person, num = line.strip().split(' ')
+            image_names_in_split.add('%s_%s.jpg' % (person, num.zfill(4)))
+    return image_names_in_split
+
+
+def _get_set_membership(train_set, test_set, valid_set, image_name):
+    if image_name in train_set:
+        return datasets.Dataset.SPLIT_KEYS['training']
+    elif image_name in test_set:
+        return datasets.Dataset.SPLIT_KEYS['test']
+    elif image_name in valid_set:
+        return datasets.Dataset.SPLIT_KEYS['validation']
+    else:
+        return None
+
 
 def configure_faces(datasets_dir):
-    pass
+    faces_dir = datasets_dir / 'Faces'
+    faces_dir.mkdir(exist_ok=True)
+
+    get_file(
+        fname='images.tgz',
+        origin='http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz',
+        cache_subdir=faces_dir.absolute(),
+        extract=True,
+        file_hash='1b42dfed7d15c9b2dd63d5e5840c86ad'
+    )
+
+    get_file(
+        fname='labels.tgz',
+        origin=('http://vis-www.cs.umass.edu/lfw/part_labels'
+                '/parts_lfw_funneled_gt_images.tgz'),
+        cache_subdir=faces_dir.absolute(),
+        extract=True,
+        file_hash='3e7e26e801c3081d651c8c2ef3c45cfc'
+    )
+
+    train_file = get_file(
+        fname='train.txt',
+        origin='http://vis-www.cs.umass.edu/lfw/part_labels/parts_train.txt',
+        cache_subdir=faces_dir.absolute()
+    )
+
+    test_file = get_file(
+        fname='test.txt',
+        origin='http://vis-www.cs.umass.edu/lfw/part_labels/parts_test.txt',
+        cache_subdir=faces_dir.absolute()
+    )
+
+    valid_file = get_file(
+        fname='valid.txt',
+        origin=('http://vis-www.cs.umass.edu/lfw/part_labels/parts_validation'
+                '.txt'),
+        cache_subdir=faces_dir.absolute()
+    )
+
+    default_images_path = faces_dir / 'lfw_funneled'
+    images_path = faces_dir / 'images'
+
+    default_labels_path = faces_dir / 'parts_lfw_funneled_gt_images'
+    labels_path = faces_dir / 'labels'
+
+    if not images_path.exists():
+        default_images_path.rename(images_path)
+
+    labels_path.mkdir(exist_ok=True)
+    if default_labels_path.exists():
+        for ppm_label in default_labels_path.iterdir():
+            # Argh why are there these hidden files?
+            if ppm_label.name[0] == '.':
+                continue
+            ppm_label_name, ppm_label_extension = splitext(ppm_label.name)
+            Image.open(ppm_label).save(
+                labels_path / ('%s.jpg' % ppm_label_name))
+
+    paths_path = faces_dir / 'paths.txt'
+    splits_path = faces_dir / 'train_val_test_split.txt'
+
+    if not paths_path.exists():
+        all_image_names = []
+        for person_subdir in images_path.iterdir():
+            if not person_subdir.is_dir():
+                continue
+            for image in person_subdir.iterdir():
+                all_image_names.append(image.name)
+        all_image_names = sorted(all_image_names)
+        with open(paths_path, 'w+') as writer:
+            for image_pair in enumerate(all_image_names):
+                writer.write('%s %s\n' % image_pair)
+
+        train_set = _read_face_split(train_file)
+        test_set = _read_face_split(test_file)
+        valid_set = _read_face_split(valid_file)
+
+        with open(splits_path, 'w+') as writer:
+            for (i, image_name) in enumerate(all_image_names):
+                set_num = _get_set_membership(train_set, test_set, valid_set,
+                                              image_name)
+                if not set_num:
+                    continue
+                writer.write('%d %d\n' % (i, set_num))
+
+    _rm_dirs([default_labels_path, default_images_path])
 
 
 def configure_birds(datasets_dir):
@@ -141,13 +259,14 @@ def configure_birds(datasets_dir):
         cache_subdir=birds_dir.absolute()
     )
 
-    default_image_path = birds_dir / 'CUB_200_2011' / 'images'
+    default_extract_dir = birds_dir / 'CUB_200_2011'
+    default_image_path = default_extract_dir / 'images'
     image_path = birds_dir / 'images'
 
     default_segmentations_path = birds_dir / 'segmentations'
     segmentations_path = birds_dir / 'labels'
 
-    default_paths_path = birds_dir / 'CUB_200_2011' / 'images.txt'
+    default_paths_path = default_extract_dir / 'images.txt'
     paths_path = birds_dir / 'paths.txt'
 
     if not image_path.exists():
@@ -157,21 +276,23 @@ def configure_birds(datasets_dir):
     if not paths_path.exists():
         default_paths_path.rename(paths_path)
 
+    _rm_dirs([default_extract_dir, default_segmentations_path])
 
-datasets_dir = Path('Datasets')
-datasets_dir.mkdir(exist_ok=True)
+
+root_dataset_dir = Path('Datasets')
+root_dataset_dir.mkdir(exist_ok=True)
 
 download_args = parse_download_args()
 
 if not download_args.flowers and \
         not download_args.faces and \
         not download_args.birds:
-    configure_birds(datasets_dir)
-    configure_faces(datasets_dir)
-    configure_flowers(datasets_dir)
+    configure_birds(root_dataset_dir)
+    configure_faces(root_dataset_dir)
+    configure_flowers(root_dataset_dir)
 elif download_args.birds:
-    configure_birds(datasets_dir)
+    configure_birds(root_dataset_dir)
 elif download_args.faces:
-    configure_faces(datasets_dir)
+    configure_faces(root_dataset_dir)
 elif download_args.flowers:
-    configure_flowers(datasets_dir)
+    configure_flowers(root_dataset_dir)
