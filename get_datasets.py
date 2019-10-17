994510c881d81c858a1dc4fc0355482d1a8884f6
Bas Straathof <bas_straathof@msn.com>
2019-10-17 11:16:09 +0200

    WIP reformat get_datasets.py to make formatting of codebase more consistent

--- get_datasets.py
+++ get_datasets.py
@@ -27,7 +27,7 @@ In paths.txt there are two fields per line, the number of the image and the
 path assuming the common structure shown above. Though differences may appear in
 each subclass of dataset.Dataset.
 
-For the NeurIPS Reproducibility Challange and the DD2412 Deep Learning, Advanced
+For the NeurIPS Reproducibility Challenge and the DD2412 Deep Learning, Advanced
 course at KTH Royal Institute of Technology.
 """
 
@@ -43,6 +43,7 @@ from sys import argv
 from pathlib import Path
 from shutil import rmtree
 from os.path import splitext
+from typing import List
 
 import datasets
 
@@ -58,13 +59,23 @@ def parse_download_args():
     return parser.parse_args(argv[1:])
 
 
-def _rm_dirs(dirs_to_rm):
+def _rm_dirs(dirs_to_rm: List):
+    """Directories to be removed
+
+    Args:
+        dirs_to_rm: List of directories to be removed
+    """
     for a_dir in dirs_to_rm:
         if a_dir.exists():
             rmtree(a_dir)
 
 
-def configure_flowers(datasets_dir):
+def configure_flowers(datasets_dir: str):
+    """Configure the Flowers data set
+
+    Args:
+        datasets_dir: Directory in which to configure the Flowers data set
+    """
     flowers_dir = datasets_dir / 'Flowers'
     flowers_dir.mkdir(exist_ok=True)
 
@@ -73,26 +84,23 @@ def configure_flowers(datasets_dir):
         origin=('http://www.robots.ox.ac.uk/~vgg/data/flowers/102/'
                 '102flowers.tgz'),
         cache_subdir=flowers_dir.absolute(),
-        extract=True
-    )
+        extract=True)
 
     get_file(
         fname='102segmentations.tgz',
         origin=('http://www.robots.ox.ac.uk/~vgg/data/flowers/102/'
                 '102segmentations.tgz'),
         cache_subdir=flowers_dir.absolute(),
-        extract=True
-    )
+        extract=True)
 
     flowers_split_download = get_file(
         fname='setid.mat',
         origin='http://www.robots.ox.ac.uk/~vgg/data/flowers/102/setid.mat',
-        cache_subdir=flowers_dir.absolute()
-    )
+        cache_subdir=flowers_dir.absolute())
 
     flowers_splits = loadmat(flowers_split_download)
 
-    # dump to file
+    # Dump to file
     add_to_inds = lambda inds, type, ids: inds.extend(
         [(id, type) for id in ids])
 
@@ -122,7 +130,7 @@ def configure_flowers(datasets_dir):
         paths = [f.name.split('_')[1] for f in sorted(images_path.iterdir())]
         with open(paths_path, 'w+') as f:
             for (i, path) in enumerate(paths):
-                f.write('%i %s\n' % (i + 1, path))
+                f.write(f'{i+1} {path}\n')
 
     default_labels_path = flowers_dir / 'segmim'
     labels_path = flowers_dir / 'labels'
@@ -135,12 +143,17 @@ def configure_flowers(datasets_dir):
     _rm_dirs([default_images_path, default_labels_path])
 
 
-def _read_face_split(fname):
+def _read_face_split(fname: str):
+    """Read face splits
+
+    Args:
+        fname: File name
+    """
     image_names_in_split = set()
     with open(fname, 'r') as reader:
         for line in reader.readlines():
             person, num = line.strip().split(' ')
-            image_names_in_split.add('%s_%s.jpg' % (person, num.zfill(4)))
+            image_names_in_split.add(f'{person}_{num.zfill(4)}.jpg')
     return image_names_in_split
 
 
@@ -164,8 +177,7 @@ def configure_faces(datasets_dir):
         origin='http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz',
         cache_subdir=faces_dir.absolute(),
         extract=True,
-        file_hash='1b42dfed7d15c9b2dd63d5e5840c86ad'
-    )
+        file_hash='1b42dfed7d15c9b2dd63d5e5840c86ad')
 
     get_file(
         fname='labels.tgz',
@@ -173,27 +185,23 @@ def configure_faces(datasets_dir):
                 '/parts_lfw_funneled_gt_images.tgz'),
         cache_subdir=faces_dir.absolute(),
         extract=True,
-        file_hash='3e7e26e801c3081d651c8c2ef3c45cfc'
-    )
+        file_hash='3e7e26e801c3081d651c8c2ef3c45cfc')
 
     train_file = get_file(
         fname='train.txt',
         origin='http://vis-www.cs.umass.edu/lfw/part_labels/parts_train.txt',
-        cache_subdir=faces_dir.absolute()
-    )
+        cache_subdir=faces_dir.absolute())
 
     test_file = get_file(
         fname='test.txt',
         origin='http://vis-www.cs.umass.edu/lfw/part_labels/parts_test.txt',
-        cache_subdir=faces_dir.absolute()
-    )
+        cache_subdir=faces_dir.absolute())
 
     valid_file = get_file(
         fname='valid.txt',
         origin=('http://vis-www.cs.umass.edu/lfw/part_labels/parts_validation'
                 '.txt'),
-        cache_subdir=faces_dir.absolute()
-    )
+        cache_subdir=faces_dir.absolute())
 
     default_images_path = faces_dir / 'lfw_funneled'
     images_path = faces_dir / 'images'
@@ -255,23 +263,20 @@ def configure_birds(datasets_dir):
             'http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_'
             '2011.tgz'),
         cache_subdir=birds_dir.absolute(),
-        extract=True
-    )
+        extract=True)
 
     get_file(
         fname='segments.tgz',
         origin=('http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/'
                 'segmentations.tgz'),
         cache_subdir=birds_dir.absolute(),
-        extract=True
-    )
+        extract=True)
 
     get_file(
         fname='train_val_test_split.txt',
         origin=('https://raw.githubusercontent.com/mickaelChen/ReDO/master/'
                 'datasplits/cub/train_val_test_split.txt'),
-        cache_subdir=birds_dir.absolute()
-    )
+        cache_subdir=birds_dir.absolute())
 
     default_extract_dir = birds_dir / 'CUB_200_2011'
     default_image_path = default_extract_dir / 'images'
