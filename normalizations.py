afd2d881e893e7b82965a337b0c8ae42b20eb57f
matssteinweg <mats.steinweg@me.com>
2019-10-26 13:18:49 +0200

    Update normalizations.py

--- normalizations.py
+++ normalizations.py
@@ -20,7 +20,7 @@ from __future__ import print_function
 
 import logging
 import tensorflow as tf
-from tensorflow_addons.utils import keras_utils
+import keras_utils
 
 
 @keras_utils.register_keras_custom_object
